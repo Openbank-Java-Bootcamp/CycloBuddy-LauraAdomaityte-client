5590f329cfea28e37cbadc8c6cee8fea12eb59e0
Laura Adomaityte <100992236+LauraAdom@users.noreply.github.com>
2022-06-16 09:01:56 +0200

    video in main page (#16)

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Layout } from "antd";
 import pic from "../images/pic1.jpg";
 import Sidebar from "../components/Sidebar";
-import pic1 from "../images/pic2.jpg";
+import video from "../images/main-video.mp4";
 import { AuthContext } from "../context/auth.context";
 const { Content } = Layout;
 
@@ -16,8 +16,10 @@ function HomePage() {
         <div className="Mainpage">
         <Sidebar />
         <div>
-          <h3 className="top-right-main">Welcome to CycloBuddy!</h3>
-          <img className="Mainpage-pic" src={pic1} alt="cyclists pic" />
+           <h3 className="top-right-main-text">Welcome to CycloBuddy!</h3> 
+          <video autoPlay muted className="homepage-video"><source src={video} /></video>
+         
+          
         </div>
       </div>
       )}
