c53b4487178a588b54ac010f2a05a9c581d575a2
LauraAdom <laura.adomaityt@gmail.com>
2022-06-17 13:50:48 +0200

    last update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer(props) {
   return (
     <div>
       
-        <footer>
+        <footer className="footer">
           <h5 className="footerText">Get in touch</h5>
           <h6 className="footerText">laura.adomaityt@gmail.com</h6>
           <h6 className="footerText">+34 631 416 031</h6>
